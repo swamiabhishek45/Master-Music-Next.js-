94b2697eb26c82818c76237623fb184e3ca595bf
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-07-01 18:45:36 +0530

    First commit

---
+++ src/app/demo/innerdemo/page.tsx
@@ -0,0 +1,5 @@
+export default function InnerDemoPage(){
+    return (
+        <h1 className="text-center">Inner Demo Page</h1>
+    )
+}
---
+++ src/app/demo/page.tsx
@@ -0,0 +1,5 @@
+export default function DemoPage(){
+    return(
+        <h1 className="text-center mt-7 text-2xl">Demo Page</h1>
+    )
+}
