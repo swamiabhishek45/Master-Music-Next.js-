35b0ab66246e5a67e811f8d041cc26ad4f00f0be
Abhishek Swami <abhishekswami1435@gmail.com>
2024-10-06 19:27:30 +0530

    update hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,19 +6,20 @@ export default function HeroSection() {
     return (
         <>
             <div className="h-auto md:h-[45rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
-            <Spotlight
-                className="-top-40 left-0 md:left-60 md:-top-20"
-                fill="white"
-            />
-                <div className="p-4 relative z-10 w-full text-center">
-                    <h1 className="mt-20 md:mt-0 w-full text-4xl md:text-7xl bg-clip-text text-transparent font-bold bg-gradient-to-b from-neutral-50 to-neutral-400">
-                        Master the Art of Music
+                <Spotlight
+                    className="-top-40 left-0 md:left-60 md:-top-20"
+                    fill="white"
+                />
+                <div className="p-20 md:p-4 relative z-10 w-full text-center">
+                    <h1 className="mt-20 md:mt-0 w-full text-3xl md:text-5xl bg-clip-text text-transparent font-bold bg-gradient-to-b from-neutral-50 to-neutral-400">
+                        LearnNex: Empowering your Tech <span className="underline text-violet-300">Dreams</span> to
+                        Reality
                     </h1>
                     <p className="py-5 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Vel et aut minima rem dolore sed optio velit, aliquam
-                        esse numquam, reiciendis, reprehenderit libero dolores
-                        nihil. At vitae dicta iusto{" "}
+                        Welcome to LearnNex, where innovation meets education,
+                        and aspirations turn into achievements. Embark on a
+                        transformative journey that propels you towards your
+                        dream career in the world of technology.
                     </p>
                     <div>
                         <Link href={"/courses"}>
@@ -26,7 +27,7 @@ export default function HeroSection() {
                                 borderRadius="1.75rem"
                                 className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
                             >
-                                Explore courses
+                                Find Programs
                             </Button>
                         </Link>
                     </div>
