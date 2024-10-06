882c75f62e5ec986630d02d37838b8e63c41e12f
Abhishek Swami <abhishekswami1435@gmail.com>
2024-10-06 19:29:03 +0530

    ADD: companies

--- src/components/MovingCards.tsx
+++ src/components/MovingCards.tsx
@@ -2,11 +2,14 @@
 
 import React, { useEffect, useState } from "react";
 import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
+import { image } from "framer-motion/client";
 
 export function InfiniteMovingCardsDemo() {
     return (
         <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
-            <h2 className="uppercase text-3xl font-bold m-10">Here Our Harmony : Voices of success</h2>
+            <h2 className="uppercase text-3xl font-bold m-10">
+                Authorized by top companies around the world
+            </h2>
             <InfiniteMovingCards
                 items={testimonials}
                 direction="right"
@@ -18,28 +21,33 @@ export function InfiniteMovingCardsDemo() {
 
 const testimonials = [
     {
-        quote: "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-        name: "Charles Dickens",
-        title: "A Tale of Two Cities",
-    },
-    {
         quote: "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
         name: "William Shakespeare",
         title: "Hamlet",
+        image: "/companies/2.png",
     },
     {
         quote: "All that we see or seem is but a dream within a dream.",
         name: "Edgar Allan Poe",
         title: "A Dream Within a Dream",
+        image: "/companies/3.png",
     },
     {
         quote: "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
         name: "Jane Austen",
         title: "Pride and Prejudice",
+        image: "/companies/4.png",
     },
     {
         quote: "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
         name: "Herman Melville",
         title: "Moby-Dick",
+        image: "/companies/5.png",
+    },
+    {
+        quote: "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
+        name: "Herman Melville",
+        title: "Moby-Dick",
+        image: "/companies/6.png",
     },
 ];
