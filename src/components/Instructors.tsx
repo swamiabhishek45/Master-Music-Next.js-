b087ed40793b250d44de04cd288b26d64323f7f6
Abhishek Swami <abhishekswami1435@gmail.com>
2024-10-07 06:59:53 +0530

    change

--- src/components/Instructors.tsx
+++ src/components/Instructors.tsx
@@ -47,7 +47,7 @@ export function Instructors() {
         <div className="relative h-[40rem] overflow-hidden flex items-center justify-center">
             <WavyBackground className="w-full max-w-7xl mx-auto flex flex-col items-center justify-center h-full">
                 <h2 className="text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center mb-8"> Meet Our Instructors</h2>
-                <p className="text-base md:text-lg text-white text-center mb-4">Discover the talented prefessional who will guide yout musical journey. </p>
+                <p className="text-base md:text-lg text-white text-center mb-4">Discover the talented prefessional who will guide you in your placement journey </p>
                 <div className="flex flex-row justify-center items-center mb-10 w-full">
                     <AnimatedTooltip items={instructors}/>
                 </div>
